06555469fa5f39846bfcb21c7ff038fcecd22a64
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-05-29 23:15:04 +0200

    Add new method in class Product construktor renderInMenu

--- src/js/script.js
+++ src/js/script.js
@@ -54,12 +54,18 @@
 
   class Product {
     constructor(id, data){
+
       const thisProduct = this;
-      console.log('new Product', thisProduct);
 
       thisProduct.id = id;
       thisProduct.data = data;
 
+      thisProduct.renderInMenu();
+
+      console.log('new Product', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
     }
   }
 
