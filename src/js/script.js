525db7a8c89c0dec05ba8da19eb84c599e4ab8df
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-05-31 21:44:27 +0200

    Add start of the initOrderForm and processOrder methods

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product', thisProduct);
     }
@@ -128,11 +130,33 @@
 
     }
 
-    initOrderForm(){};
-    processOrder(){};
+    initOrderForm() {
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function () {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    };
+    processOrder() {
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData',formData);
+    };
 
   }
-
 
 
   const app = {
