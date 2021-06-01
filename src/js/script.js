c249e43a81c917a75820eb0522ae2aa9a5c686b8
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-06-01 21:23:50 +0200

    Add conditions for the price calculation

--- src/js/script.js
+++ src/js/script.js
@@ -166,22 +166,34 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        console.log('paramId', paramId, 'param', param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          console.log('optionID', optionId, 'option', option);
+
+          // checking paramId in formData and if there is an optionId
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+
+            // default state of the option - !option - not default
+            if (!option.default == true) {
+              //increase price
+              price += option.price;
+            }
+          } else {
+            // if default
+            if (option.default == true) {
+              // reduce price 
+              price -= option.price;
+            }
+          }
         }
       }
-
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
-
-
     }
-
   }
 
 
