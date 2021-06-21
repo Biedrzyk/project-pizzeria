ddcec7bd1875771fcb96f64e9ba0134c19a2bc13
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-06-21 21:13:34 +0200

    add new method render in Booking class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,9 +1,27 @@
-
+import { templates, select, settings, classNames } from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
 
 class Booking {
   constructor (element) {
     const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
   }
+
+  render (element) {
+    const thisBooking = this;
+
+    const generateHTML = templates.bookingWidget;
+    
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generateHTML;
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+  }
+
 }
 
 export default Booking;
