ddc5757fef24dce35bcb826ac3d2f0f3c9714aff
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-06-22 21:23:33 +0200

    add new content from the excersise to the BaeWidget class

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,14 +1,14 @@
 import { select, settings } from '../settings.js';
 import BaseWidget from './BaseWidget.js';
 
-class AmountWidget extends BaseWidget{  // klasa AmountWidget jest rozszerzeniem klasy BaseWidget
+class AmountWidget extends BaseWidget {  // klasa AmountWidget jest rozszerzeniem klasy BaseWidget
   constructor(element) {                // pierwsza rzecz jaką robimy w konstruktorze klasy dziedziczącej jest wywołanie konstruktora klasy nadrzędnej super();
 
     super(element, settings.amountWidget.defaultValue); // oznacza konstruktor klasy BaseWidget
 
     const thisWidget = this;
 
-    console.log('AmountWidget: ', AmountWidget);
+    console.log('AmountWidget: ', thisWidget);
     console.log('constructor arguments: ', element);
 
 
@@ -27,32 +27,11 @@ class AmountWidget extends BaseWidget{  // klasa AmountWidget jest rozszerzeniem
     thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  setValue(value) {
 
-    const thisWidget = this;
-
-    const newValue = thisWidget.parseValue(value); // parseValue jest metodą thisWidget
-
-
-    /* TODO: Add validation */  // != różni się
-    if (newValue != thisWidget.value && !isNaN(newValue) && thisWidget.isValid(newValue)) {
-      thisWidget.value = newValue;
-    }
-    thisWidget.renderValue();
-    thisWidget.announce();
-  }
-
-  parseValue(value) {
-
-    return parseInt(value);
-  }
-
-
-  isValid(value) {    // będzie zwracaćprawdęlub fałsz w zależności od kryteriów które ustalimy dla widgetów
-
+  isValid(value) {    // będzie zwracać prawdę lub fałsz w zależności od kryteriów które ustalimy dla widgetów
     return !isNaN(value)  // jeśli value jest tekstem to isNaN jest prawdziwa
-    && value >= settings.amountWidget.defaultMin 
-    && value <= settings.amountWidget.defaultMax;
+      && value >= settings.amountWidget.defaultMin
+      && value <= settings.amountWidget.defaultMax;
   }
 
   renderValue() { // bieżąca wartość widgetu będzie wyświetlona na stronie
@@ -77,18 +56,6 @@ class AmountWidget extends BaseWidget{  // klasa AmountWidget jest rozszerzeniem
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
-
-
-
-  }
-
-  announce() {
-    const thisWidget = this;
-
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-    thisWidget.dom.wrapper.dispatchEvent(event);
   }
 }
 
--- src/js/components/BaseWIdget.js
+++ src/js/components/BaseWIdget.js
@@ -1,11 +1,58 @@
-class BaseWidget {
+class BaseWidget {  // ma byćsamodzielna więc będzie korzystaćtylko z metod w niej zawartych
   constructor(wrapperElement, initialValue){
     const thisWidget = this;
     
     thisWidget.dom = {};
     thisWidget.dom.wrapper = wrapperElement;
 
-    thisWidget.value = initialValue;
+    thisWidget.value = initialValue; // zmieniamy na value żeby odpalił się poprawnie setter set value
+  }
+
+  get value() {   // getter sprawdza jaka jest wartość, metoda wykonywana przy każdej próbie odczytania wartości właściwości value
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+
+  set value(value) { // setter wpisuje nową wartość, metoda wykonywana przy każdej próbie ustawienia nowej wartości właściwości value
+
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(value); // parseValue jest metodą thisWidget
+
+
+    /* TODO: Add validation */  // != różni się
+    if (newValue != thisWidget.correctValue && !isNaN(newValue) && thisWidget.isValid(newValue)) {
+      thisWidget.correctValue = newValue;
+    }
+    thisWidget.renderValue();
+    thisWidget.announce();
+  }
+
+  parseValue(value) { // skasujemy parseValue w klasie AmountWidget bo tu jest identyczna
+
+    return parseInt(value);
+  }
+
+
+  isValid(value) {    // isValid zostaje w obu klasach bo się różnią
+
+    return !isNaN(value);  // jeśli value jest tekstem to isNaN jest prawdziwa
+  }
+
+  renderValue() { // bieżąca wartość widgetu będzie wyświetlona na stronie
+    const thisWidget = this;
+
+    thisWidget.dom.wrapper.innerHTML = thisWidget.value; // zmieniamy na wrapper bo nie wiadomo czy będzie input czy nie
+  }
+
+  announce() {    // przenosimy z AmountWidget bo nie ma specyficznych informacji dla widgetu ilości, jest uniwersalna
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.dom.wrapper.dispatchEvent(event);
   }
 }
 
