5e7d86fcbb81f060d55e265df6b56e4e895a0128
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-06-23 20:49:58 +0200

    Iport of the HourPicker and DatePicker tin the Booking.js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor (element) {
@@ -28,8 +30,8 @@ class Booking {
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
-    thisBooking.datePicker = new AmountWidget(thisBooking.dom.datePicker);
-    thisBooking.hourPicker = new AmountWidget(thisBooking.dom.hourPicker);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
 
